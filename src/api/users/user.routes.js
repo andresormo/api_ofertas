da0c810e97a7540aef7edf9aa4839ce909886610
andres orozco moreno <andresormo@gmail.com>
2023-04-23 21:20:35 +0200

    antes de la entrega

--- src/api/users/user.routes.js
+++ src/api/users/user.routes.js
@@ -1,12 +1,12 @@
 const{signUp, login, modifyUser, getAllUser, getUserById} = require("./user.controller");
 const { isAuth, isAdmin} = require("../../middlewares/auth");
-
+const upload = require("../../middlewares/file");
 const userRouter = require("express").Router();
 
 userRouter.get("/", getAllUser);
 userRouter.get("/:id", getUserById)
 userRouter.post("/", signUp);
-userRouter.post("/login",login);
+userRouter.post("/login",login, upload.single('portada'));
 userRouter.put("/:id",[isAuth], modifyUser);
 
 module.exports = userRouter;
