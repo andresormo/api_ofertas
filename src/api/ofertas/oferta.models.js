c138af0f8d0b31f2e2d04c310716c7332666d931
andres orozco moreno <andresormo@gmail.com>
2023-04-23 11:52:05 +0200

    cambio portada

--- src/api/ofertas/oferta.models.js
+++ src/api/ofertas/oferta.models.js
@@ -8,7 +8,7 @@ const ofertaSchema = new mongoose.Schema(
         descripcion:{type:String},
         contrato:{type:String, enum:["indefinido", "temporal", "larga duración", "sustitucion", "baja"]},
         sector:{type:String, enum:["Administracion","Tecnologico", "Agrícola", "Servicio", "Turismo", "Industria", "Otros"]},
-        portada:{type:String, default:"https://cdn-icons-png.flaticon.com/512/25/25634.png"},
+        portada:{type:Object, default:"https://cdn-icons-png.flaticon.com/512/25/25634.png"},
         telefono:{type:String, required:true},
         email:{type:String, required:true},
         ubicacion:{type:String}
